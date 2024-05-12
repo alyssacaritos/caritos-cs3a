bdbd107633d3aeb52b883c5d506455b7b7bf3da0
Alyssa Caritos <alyssapinocaritos@gmail.com>
2024-05-12 23:09:53 +0000

    update

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import importlib.util
 import os
 from streamlit.logger import get_logger
-from pages import BlockCipher_XOR, Hashing_function, XOR_Cipher, Caesar_Cipher, SecureChatwithDiffieHellman # Importing the Block Cipher and Hashing function files
+from pages import BlockCipher_XOR, Hashing_function  # Importing the Block Cipher and Hashing function files
 
 LOGGER = get_logger(__name__)
 
@@ -15,20 +15,26 @@ def execute_py_file(file_path):
 # Define the list of available .py files with their respective names and functions
 file_functions = {
     "BlockCipher_XOR.py": (BlockCipher_XOR.block_cipher_xor, "Block Cipher XOR"),
-    "XOR_Cipher.py": (XOR_Cipher.main),
-    "Caesar_Cipher.py": (Caesar_Cipher.main),
-    "Hashing_function.py": (Hashing_function.main),
-    "SecureChatwithDiffieHellman.py": (SecureChatwithDiffieHellman)
+    "XOR_Cipher.py": (None, "XOR Cipher"),
+    "Caesar_Cipher.py": (None, "Caesar Cipher"),
+    "Hashing_function.py": (Hashing_function.main, "Hashing Function"),
+    "SecureChatwithDiffie-Hellman.py": (None, "Secure Chat with Diffie-Hellman")
 }
+
+# Debugging prints to check the structure of file_functions dictionary
+print("file_functions:", file_functions)
+
+# Extract the names of the pages
+page_names = [name for function, name in file_functions.values()]
 
 # Display buttons for each .py file in the sidebar
 st.sidebar.title("📚 Pages")
-selected_page = st.sidebar.radio("", [name for function, name in file_functions.values()], index=0)
+selected_page = st.sidebar.radio("", page_names, index=0)
 
 # Show content of the selected page
 st.title("🚀 Streamlit Page Navigator")
 
-if selected_page in [name for function, name in file_functions.values()]:
+if selected_page in page_names:
     function_to_execute = [function for function, name in file_functions.values() if name == selected_page][0]
     if function_to_execute:
         st.markdown(f"## {selected_page}")
