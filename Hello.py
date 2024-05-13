fbfa564a96f6f6f59e9023840f5ee51928b6c53c
Alyssa Caritos <alyssapinocaritos@gmail.com>
2024-05-13 00:11:05 +0000

    update

--- Hello.py
+++ Hello.py
@@ -32,7 +32,12 @@ show_text_only = st.checkbox("View Text Only")
 
 if show_text_only:
     st.write("""
-        **Author**: ![color=#FF5733]Caritos, Alyssa P. ![color=#58D68D]Maraño, Mary France ![color=#3498DB]Aguilar, Troy
+        
+        **📝 Author**: 
+            
+            Caritos, Alyssa P.
+            Maraño, Mary France
+            Aguilar, Troy
 
         The Applied Cryptography Application project aims to develop a simple application that implements various cryptographic techniques to secure communication, data, and information exchange. Cryptography is the science of encoding and decoding messages to protect their confidentiality, integrity, and authenticity. 
 
